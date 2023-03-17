9d37e9f9ad3d4259d080edfcac89832352b6885d
Pavel Narzyaev <pavel@narzyaev.ru>
2023-03-18 01:23:49 +0300

    Images scripts

--- Assets/Scripts/ImagePromptsBlock.cs
+++ Assets/Scripts/ImagePromptsBlock.cs
@@ -1,7 +1,38 @@
-﻿public class ImagePromptsBlock : PromptBlock
+﻿using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class ImagePromptsBlock : PromptBlock
 {
+    [FormerlySerializedAs("unusedImages")] [SerializeField] private Images notUsedImages;
+    [SerializeField] private Images usedImages;
+
+    public void Start()
+    {
+        notUsedImages.Initialize(new List<ImageDto>
+        {
+            new()
+            {
+                link = "link1"
+            },
+            new()
+            {
+                link = "link2"
+            }
+        });
+    }
+
     public override string GetResultString()
     {
-        return "images";
+        var resultStringBuilder = new StringBuilder();
+        foreach (var image in notUsedImages.GetImages())
+        {
+            if (resultStringBuilder.Length != 0)
+                resultStringBuilder.Append(" ");
+            resultStringBuilder.Append(image.link);
+        }
+
+        return resultStringBuilder.ToString();
     }
 }
---
+++ Assets/Scripts/Images.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Images : MonoBehaviour
+{
+    private List<ImageDto> _images;
+
+    public void Initialize(List<ImageDto> images)
+    {
+        _images = images;
+    }
+
+    public List<ImageDto> GetImages()
+    {
+        return _images;
+    }
+}
