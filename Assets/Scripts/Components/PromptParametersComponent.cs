83e0b3fd640876a7cb788b87f3c9aa92c501e28a
Pavel Narzyaev <pavel@narzyaev.ru>
2023-03-19 13:25:01 +0300

    Parameters

--- Assets/Scripts/Components/PromptParametersComponent.cs
+++ Assets/Scripts/Components/PromptParametersComponent.cs
@@ -1,5 +1,13 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class PromptParametersComponent : MonoBehaviour
 {
+    [SerializeField] private TMP_InputField inputField;
+
+    private void Start()
+    {
+        inputField.text = Model.PromptPartParameters.GetPromptPart();
+        inputField.onValueChanged.AddListener(value => Model.PromptPartParameters.SetText(value));
+    }
 }
--- Assets/Scripts/Model/PromptPartParametersModel.cs
+++ Assets/Scripts/Model/PromptPartParametersModel.cs
@@ -2,15 +2,22 @@
 
 public class PromptPartParametersModel : PromptPartModelBase
 {
+    private string _text;
     public override event Action changeEvent;
 
     public PromptPartParametersModel()
     {
-        
+        _text = "--v 5 --iw 0.8 --s 500 --uplight --c 0";
+    }
+
+    public void SetText(string value)
+    {
+        _text = value;
+        changeEvent?.Invoke();
     }
 
     public override string GetPromptPart()
     {
-        return "PARAMETERS";
+        return _text;
     }
 }
