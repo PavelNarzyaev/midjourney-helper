d8fe2878f855ba89b812f979930b5c5d6c292d22
Pavel Narzyaev <pavel@narzyaev.ru>
2023-04-01 20:20:17 +0300

    Bottom animation

--- Assets/Scripts/Components/ResultPromptComponent.cs
+++ Assets/Scripts/Components/ResultPromptComponent.cs
@@ -1,4 +1,5 @@
-﻿using TMPro;
+﻿using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,12 +7,46 @@ public class ResultPromptComponent : MonoBehaviour
 {
     [SerializeField] private TMP_Text text;
     [SerializeField] private Button button;
+    [SerializeField] private CanvasGroup animationCanvasGroup;
 
     private void Start()
     {
         Model.ResultPrompt.changeEvent += RefreshText;
         button.onClick.AddListener(Model.ResultPrompt.CopyToClipboard);
         RefreshText();
+        StartCoroutine(AnimationRoutine());
+    }
+
+    // ReSharper disable once IteratorNeverReturns
+    private IEnumerator AnimationRoutine()
+    {
+        var targetAlpha = 0f;
+        var animationSteps = 0;
+        var currentStep = 0;
+        var increasingDirection = animationCanvasGroup.alpha == 0;
+
+        do
+        {
+            if (currentStep >= animationSteps)
+            {
+                currentStep = 0;
+                var randomMultiplier = Random.Range(0.1f, 1f);
+                if (increasingDirection)
+                    targetAlpha = (1 - animationCanvasGroup.alpha) * randomMultiplier;
+                else
+                    targetAlpha = animationCanvasGroup.alpha * randomMultiplier;
+                animationSteps = 480 + Random.Range(0, 60);
+                increasingDirection = !increasingDirection;
+            }
+
+            var alpha = animationCanvasGroup.alpha;
+            var alphaDistance = targetAlpha - alpha;
+            var steps = animationSteps - currentStep;
+            alpha += alphaDistance / steps;
+            animationCanvasGroup.alpha = alpha;
+            currentStep++;
+            yield return null;
+        } while (true);
     }
 
     private void RefreshText()
